79f8b1d95b1958cb56ea86ae3a4c725ddb641fac
Avazbek <ax1of710@gmail.com>
2024-12-13 09:12:11 +0500

    push

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 import asyncio
